a56b8a9a8c5b1823fecbe64505c15e8059439da7
hiromsbc <hiro159m@gmail.com>
2025-05-04 16:03:09 +0900

    HomePageへの動線作成、ページ作成

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <Container>
+                <div>
+                    Home Page
+                </div>
+            </Container>
+        )
+    }
+}
+
+
+
