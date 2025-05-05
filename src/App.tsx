ee0cbc6f4dce6c1d439045eba7f23772095f04d6
hiromsbc <hiro159m@gmail.com>
2025-05-05 10:58:16 +0900

    トップバーの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './components/pages/Home'
+import { FrontVar } from './components/common/FrontVar'
 
 
 function App() {
 
   return (
     <BrowserRouter>
+      <FrontVar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
