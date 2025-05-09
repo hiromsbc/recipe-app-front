2b652332df5a6a3e4d4b48a5e3e050bdc33d9b66
hiromsbc <hiro159m@gmail.com>
2025-05-10 01:05:37 +0900

    ホーム画面の編集

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,39 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const Home = () => (
-    <Container>
-        <div>
-            Home Page
-        </div>
+    <Container className="py-4">
+        <Row className="g-4">
+            <Col md={4}>
+                <Card className="h-100 shadow-sm">
+                    <Card.Body className="d-flex flex-column">
+                        <Card.Title>ランキング</Card.Title>
+                        <Card.Text>
+                            アクセス上位のレシピを表示
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col md={4}>
+                <Card className="h-100 shadow-sm">
+                    <Card.Body className="d-flex flex-column">
+                        <Card.Title>新着</Card.Title>
+                        <Card.Text>
+                            新着順にレシピを表示
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col md={4}>
+                <Card className="h-100 shadow-sm">
+                    <Card.Body className="d-flex flex-column">
+                        <Card.Title>レシピ検索</Card.Title>
+                        <Card.Text>
+                            レシピ情報を検索
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     </Container>
 );
 
